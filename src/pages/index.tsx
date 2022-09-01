6417464d66b35e8137dd451f9a6367f42cfb95fa
iromonek39 <kentarou.omori0309@gmail.com>
2022-09-01 23:49:22 +0900

    環境構築

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import { css } from '@emotion/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const Title = css`
+    font-size: 44px;
+  `
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +17,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 css={Title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
