c00fe450d90fcfe53b0ed804d22e64017ba51a7b
iromonek39 <kentarou.omori0309@gmail.com>
2022-09-03 00:15:27 +0900

    layout

---
+++ src/components/layout.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { ReactNode } from 'react'
+
+type Props = {
+  children?: ReactNode
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div>
+      <Head>
+        <title>MyTemplate</title>
+      </Head>
+
+      <header className="">
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </header>
+      <div className="container">{children}</div>
+      <footer className=""></footer>
+    </div>
+  )
+}
+
+export default Layout
