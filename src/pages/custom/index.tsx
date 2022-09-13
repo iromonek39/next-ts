e8b01e91b3116d79eb29909fbf44d83ff4c1eaac
iromonek39 <kentarou.omori0309@gmail.com>
2022-09-13 14:57:17 +0900

    price, kcal

--- src/pages/custom/index.tsx
+++ src/pages/custom/index.tsx
@@ -1,43 +1,49 @@
-import { css } from '@emotion/react'
-import Link from 'next/link'
-import React from 'react'
+import { css } from "@emotion/react";
+import Link from "next/link";
+import React from "react";
 
 type Props = {
-  onClick: (event: React.MouseEvent<HTMLInputElement>) => void
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
-  onkeypress: (event: React.KeyboardEvent<HTMLInputElement>) => void
-  onBlur: (event: React.FocusEvent<HTMLInputElement>) => void
-  onFocus: (event: React.FocusEvent<HTMLInputElement>) => void
-  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void
-  onClickDiv: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
-}
+  onClick: (event: React.MouseEvent<HTMLInputElement>) => void;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onkeypress: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+  onBlur: (event: React.FocusEvent<HTMLInputElement>) => void;
+  onFocus: (event: React.FocusEvent<HTMLInputElement>) => void;
+  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  onClickDiv: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+};
 
 const index = () => {
   type Sandwich = {
-    name: string
-    id: number
-  }
+    name: string;
+    id: number;
+    price: number;
+    kcal: number;
+  };
 
   const sandwiches: Sandwich[] = [
     {
       id: 1,
-      name: 'BLT',
+      name: "BLT",
+      price: 430,
+      kcal: 330,
     },
     {
       id: 2,
-      name: 'サラダチキン',
+      name: "サラダチキン",
+      price: 430,
+      kcal: 278,
     },
     {
       id: 3,
-      name: 'チリチキン',
+      name: "チリチキン",
+      price: 430,
+      kcal: 273,
     },
-  ]
+  ];
 
   const selectedSandwich = (event: React.MouseEvent<HTMLInputElement>) => {
-    console.log(2)
-
-    console.log(event)
-  }
+    console.log(event);
+  };
 
   return (
     <>
@@ -62,14 +68,14 @@ const index = () => {
               `}
               onClick={selectedSandwich}
             >
-              {sandwich.name}
+              {sandwich.name},¥{sandwich.price}/{sandwich.kcal}kcal
             </li>
           ))}
         </ul>
       </div>
       <Link href="/">トップ</Link>
     </>
-  )
-}
+  );
+};
 
-export default index
+export default index;
